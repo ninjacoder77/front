939bc9826ccc169db9bcfc97c145dcf4f1c169d4
ninjacoder77 <ps4nenao@gmail.com>
2024-11-07 14:43:35 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://b54f-2804-68c4-9e-e800-be99-4928-3c50-59ca.ngrok-free.app',
+  apiUrl: 'https://93fc-2804-68c4-9e-e800-be99-4928-3c50-59ca.ngrok-free.app',
 };
