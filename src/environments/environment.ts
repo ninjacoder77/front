28b8f81316d1ea3a397aef834a57b1f01433208a
ninjacoder77 <ps4nenao@gmail.com>
2024-11-08 16:21:24 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://2800-2804-68c4-9e-e800-1956-2f7f-15ec-21d5.ngrok-free.app',
+  apiUrl: 'https://c7ef-2804-68c4-9e-e800-1956-2f7f-15ec-21d5.ngrok-free.app',
 };
